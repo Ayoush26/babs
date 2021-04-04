5b06750dbb1d8323d4bc86aeccfb0417e23cd88c
Ayoush Moktan Lama <aayushlama26@gmail.com>
2021-04-04 15:01:53 +0545

    'accessControl'

--- src/util/httpClient.js
+++ src/util/httpClient.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const headers = {
-    "Content-Type": 'application/json'
+    "Content-Type": 'application/json',
+    "Access-Control-Allow-Origin": "*"
 }
 
 const http = axios.create({
